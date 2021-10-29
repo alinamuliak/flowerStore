81a29030d3e9ee7542e89c23bd39abe45bdb582d
alinamuliak <alina.muliak@ucu.edu.ua>
2021-10-29 18:28:12 +0300

    added decorators

---
+++ src/main/java/decorators/RibbonDecorator.java
@@ -0,0 +1,16 @@
+package decorators;
+
+import flowerstore.Item;
+
+public class RibbonDecorator extends ItemDecorator {
+    private Item item;
+    public RibbonDecorator(Item item) {
+        super(item);
+    }
+
+    @Override
+    public double getPrice() {
+        return 40 + item.getPrice();
+    }
+}
+
