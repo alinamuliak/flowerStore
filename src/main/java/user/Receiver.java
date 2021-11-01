0b4996966f621202a67a73cebe9db530b3471e47
alinamuliak <alina.muliak@ucu.edu.ua>
2021-11-01 10:27:09 +0200

    added user classes

---
+++ src/main/java/user/Receiver.java
@@ -0,0 +1,7 @@
+package user;
+
+public class Receiver implements User {
+    public void update(boolean status) {
+        System.out.println("Receiver updated!)");
+    }
+}
---
+++ src/main/java/user/Sender.java
@@ -0,0 +1,7 @@
+package user;
+
+public class Sender implements User {
+    public void update(boolean status) {
+        System.out.println("Sender updated!)");
+    }
+}
