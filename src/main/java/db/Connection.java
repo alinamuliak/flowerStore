897023883403e2d9401aa746d0e81871f7fcbf46
alinamuliak <alina.muliak@ucu.edu.ua>
2021-11-01 11:27:31 +0200

    added Connection

---
+++ src/main/java/db/Connection.java
@@ -0,0 +1,13 @@
+package db;
+
+public class Connection {
+    public static Connection thisConnection;
+    private Connection() {}
+
+    public static Connection getConnection() {
+        if (thisConnection == null) {
+            thisConnection = new Connection();
+        }
+        return thisConnection;
+    }
+}
